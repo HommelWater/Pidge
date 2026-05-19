a60f73c18125e189a270528d3214efea5097b72a
smonnnn <slooij1@gmail.com>
2026-05-19 14:26:30 +0200

    modify setup

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.mount("/site/", StaticFiles(directory="./interface/"), name="interface")
+
+@app.get("/{full_path:path}")
+async def spa(full_path: str):
+    return FileResponse("site/index.html")
